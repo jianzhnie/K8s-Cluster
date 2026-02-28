ad73c84578304563e720ef045ce3654f2161b74d
jianzhnie <jianzhnie@gmail.com>
2026-02-28 17:49:15 +0800

    chore(scripts): add run script and parameterize checkpoint conversion
    Add a wrapper run.sh script to execute the conversion in the background and log output.
    Modify ckpt_convert_kimi2_hf2mcore.sh to use variables for tensor, pipeline, and expert parallelism settings, making the script more configurable and maintainable.

--- scripts/kimi2/ckpt_convert_kimi2_hf2mcore.sh
+++ scripts/kimi2/ckpt_convert_kimi2_hf2mcore.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # 修改 ascend-toolkit 路径
 source /usr/local/Ascend/ascend-toolkit/set_env.sh
 export CUDA_DEVICE_MAX_CONNECTIONS=1
@@ -5,13 +7,22 @@ export CUDA_DEVICE_MAX_CONNECTIONS=1
 # 权重路径需根据实际情况修改
 # load-dir: huggingface 权重路径
 # save-dir: mcore 权重保存路径
+
+tp=2
+pp=16
+ep=32
+
+model_dir="/home/robin/hfhub/models/moonshotai/"
+hf_model_dir=$model_dir/Kimi-K2-Base
+save_dir=$model_dir/Kimi-K2-Base-mcore_tp${tp}_pp${pp}_ep${ep}
+
 python examples/mcore/kimi2/convert_ckpt_kimi2.py\
     --moe-grouped-gemm \
     --target-tensor-parallel-size 2 \
     --target-pipeline-parallel-size 8 \
     --target-expert-parallel-size 32 \
-    --load-dir /home/robin/hfhub/models/moonshotai/Kimi-K2-Base \
-    --save-dir /home/robin/hfhub/models/moonshotai/Kimi-K2-Base-mcore \
+    --load-dir $hf_model_dir \
+    --save-dir $save_dir \
     --num-layers 64 \
     --noop-layers 61,62,63 \
     --num-dense-layers 1
